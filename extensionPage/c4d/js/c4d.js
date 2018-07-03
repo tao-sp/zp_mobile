b938263c480215dd99f6741748fbda978fddff1d
OU_UO <382803687@qq.com>
2018-07-03 14:43:10 +0800

    c4d

--- extensionPage/c4d/js/c4d.js
+++ extensionPage/c4d/js/c4d.js
@@ -4,4 +4,11 @@ $(function () {
         var dataFlag= $(this).attr("data-flag");
         $("."+dataFlag).addClass("imgshow").siblings().removeClass("imgshow");
     })
+
+    $(".right-nav").click(function () {
+        $(".right-nav-hide").fadeIn();
+    })
+    $(".close-nav img").click(function () {
+        $(".right-nav-hide").fadeOut();
+    })
 })
