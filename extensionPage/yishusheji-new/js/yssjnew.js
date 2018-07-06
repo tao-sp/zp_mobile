98f8e1b3ec4eb31b483aa23330820e72a713bc56
OU_UO <382803687@qq.com>
2018-07-06 14:33:37 +0800

    20180706

---
+++ extensionPage/yishusheji-new/js/yssjnew.js
@@ -0,0 +1,9 @@
+$(function () {
+
+    $(".right-nav").click(function () {
+        $(".right-nav-hide").show();
+    })
+    $(".close-nav img").click(function () {
+        $(".right-nav-hide").hide();
+    })
+})
