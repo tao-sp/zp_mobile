4ef92c937267089fefbe33b4825d1cfb3752f59b
OU_UO <382803687@qq.com>
2018-07-06 13:53:01 +0800

    艺术设计新

--- extensionPage/yssjNew/js/yssj.js
+++ extensionPage/yssjNew/js/yssj.js
@@ -28,7 +28,20 @@ $(function () {
     $(".jyfw-lists li").click(function () {
         $(this).addClass("on").siblings().removeClass("on");
       var flag =   $(this).attr("data-flag");
-      $(".jyfwswiper").css("left","500%");
-      $("."+flag).css("left","0");
+      $(".jyfwswiper").css("position","absolute");
+      $("."+flag).css("position","static");
     })
+    /*回到顶部函数；当点击跳转链接后，回到页面顶部位置 */
+    if(!$("#backTop").length){return;}
+    $("#backTop").click(function() {
+        $('body,html').animate({
+                scrollTop: 0
+            },
+            1000);
+        return false;
+    });
 })
+
+function hideright() {
+    $(".rightserve").toggle();
+}
