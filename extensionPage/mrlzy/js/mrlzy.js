e69ea0344ec33a6ba86fa6a30f7b44ab5485566d
OU_UO <382803687@qq.com>
2018-09-27 13:27:58 +0800

    学历提升方案

---
+++ extensionPage/mrlzy/js/mrlzy.js
@@ -0,0 +1,29 @@
+$(function () {
+    $(".jsmonth").text(GetDateStrm(2));
+    $(".jsday").text(GetDateStrd(2));
+    $(".jsyear").text(GetDateStry(2));
+})
+function GetDateStrm(AddDayCount){
+    var dd = new Date();
+    dd.setDate(dd.getDate()+AddDayCount);//获取AddDayCount天后的日期
+    // var y = dd.getYear();
+    var m = dd.getMonth()+1;//获取当前月份的日期
+    // var d = dd.getDate();
+    return  m;
+}
+function GetDateStrd(AddDayCount) {
+    var dd = new Date();
+    dd.setDate(dd.getDate() + AddDayCount);//获取AddDayCount天后的日期
+    // var y = dd.getYear();
+    // var m = dd.getMonth()+1;//获取当前月份的日期
+    var d = dd.getDate();
+    return d;
+}
+function GetDateStry(AddDayCount) {
+    var dd = new Date();
+    dd.setDate(dd.getDate() + AddDayCount);//获取AddDayCount天后的日期
+     var y = dd.getFullYear();
+    // var m = dd.getMonth()+1;//获取当前月份的日期
+    // var d = dd.getDate();
+    return y;
+}
