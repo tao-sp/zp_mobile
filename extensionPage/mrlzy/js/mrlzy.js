90e93bd0a39f4944d4215b0f431b530f5e62f549
OU_UO <382803687@qq.com>
2018-09-27 13:39:45 +0800

    学历提升方案

--- extensionPage/mrlzy/js/mrlzy.js
+++ extensionPage/mrlzy/js/mrlzy.js
@@ -3,21 +3,6 @@ $(function () {
     $(".jsday").text(GetDateStrd(2));
     $(".jsyear").text(GetDateStry(2));
 })
-window.onload=function () {
-    document.addEventListener('touchstart',function (event) {
-        if(event.touches.length>1){
-            event.preventDefault();
-        }
-    })
-    var lastTouchEnd=0;
-    document.addEventListener('touchend',function (event) {
-        var now=(new Date()).getTime();
-        if(now-lastTouchEnd<=300){
-            event.preventDefault();
-        }
-        lastTouchEnd=now;
-    },false)
-}
 
 function GetDateStrm(AddDayCount){
     var dd = new Date();
