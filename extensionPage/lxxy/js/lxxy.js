c232270ca8ac4bcf031a0df041c772ae6d4fb115
OU_UO <382803687@qq.com>
2018-10-09 15:19:31 +0800

    学历提升方案

--- extensionPage/lxxy/js/lxxy.js
+++ extensionPage/lxxy/js/lxxy.js
@@ -27,8 +27,20 @@ $(function () {
             $(".qrcode").hide();
         }
     })
-
-
+    $(".paper-input-gruop label").click(function () {
+        var judge = $(this).parent(".right").length; //0选择错误 1选择正确
+        if(judge==1){
+            showTips();
+            var last =  $(this).parents(".last-qustion").length;
+            nextQustion(last,2000)
+        }else {
+            $(this).parent().siblings(".right").addClass("on");
+            $(this).parent().siblings(".lxxy-paper-answer").slideDown();
+        }
+    })
+    $(".message-view").click(function () {
+        $(this).parent().next(".message-detail").slideToggle();
+    })
 })
 
 function OpenRegist(classNane,className1) {
@@ -39,4 +51,56 @@ function OpenRegist(classNane,className1) {
     $(".regist-login").hide();
     $("."+classNane).fadeIn();
     $("."+className1).fadeIn();
+}
+function nextQustion(last,time) {
+    $(".pre-qustion").removeClass("no-click");
+    $(".pre-qustion").removeAttr('disabled');
+    setTimeout(function () {
+        if(last==0){
+            var j = $(".show-qustion").next(".last-qustion").length; //1最后一题
+            if(j==1){
+                $(".next-qustion").addClass("no-click");
+                $(".next-qustion").attr('disabled',"true");
+            }
+            $(".show-qustion").removeClass("show-qustion").next().addClass("show-qustion");
+        }
+    },time)
+
+}
+function preQustion(last,time) {
+    $(".next-qustion").removeClass("no-click");
+    $(".next-qustion").removeAttr('disabled');
+    setTimeout(function () {
+        if(last==0){
+            var j = $(".show-qustion").prev(".first-qustion").length; //1第一题
+            if(j==1){
+                $(".pre-qustion").addClass("no-click");
+                $(".pre-qustion").attr('disabled',"true");
+            }
+            $(".show-qustion").removeClass("show-qustion").prev().addClass("show-qustion");
+        }
+    },time)
+
+}
+function showTips() {
+
+    $(".right-tips").animate({
+        width:"200px",
+        height:"40px",
+    },300);
+    setTimeout(function () {
+        $(".right-tips").animate({
+            width:"0",
+            height:"0"
+        },300);
+    },800)
+
+}
+
+function messageDel(obj) {
+    $(obj).parent().parent("li").animate({
+        "width":"0",
+        "height":"0",
+        "padding":"0"
+    },300)
 }
