7903e6784d2427bd73e9fafb68b9b888cda72ea5
OU_UO <382803687@qq.com>
2018-10-29 10:48:26 +0800

    学历提升方案

--- extensionPage/zppx/sy/js/sy.js
+++ extensionPage/zppx/sy/js/sy.js
@@ -51,6 +51,61 @@ $(function () {
         });
     })();
 
+    $(".sz-ctrl-list li").hover(function () {
+        $(this).addClass("on").siblings().removeClass("on");
+        var index = $(this).index();
+        $(".sz-info-lsit li").hide();
+        $(".sz-info-lsit li").eq(index).show();
+    })
+
+    certifySwiper = new Swiper('#certify .swiper-container', {
+        watchSlidesProgress: true,
+        slidesPerView: 'auto',
+        centeredSlides: true,
+        loop: true,
+        loopedSlides: 5,
+        autoplay: 3000,
+        prevButton: '.swiper-button-prev',
+        nextButton: '.swiper-button-next',
+        pagination: '.swiper-pagination',
+        //paginationClickable :true,
+        onProgress: function(swiper, progress) {
+            for (i = 0; i < swiper.slides.length; i++) {
+                var slide = swiper.slides.eq(i);
+                var slideProgress = swiper.slides[i].progress;
+                modify = 1;
+                if (Math.abs(slideProgress) > 1) {
+                    modify = (Math.abs(slideProgress) - 1) * 0.3 + 1;
+                }
+                translate = slideProgress * modify * 175 + 'px';
+                scale = 1 - Math.abs(slideProgress) / 5;
+                zIndex = 999 - Math.abs(Math.round(10 * slideProgress));
+                slide.transform('translateX(' + translate + ') scale(' + scale + ')');
+                slide.css('zIndex', zIndex);
+                slide.css('opacity', 1);
+                if (Math.abs(slideProgress) > 3) {
+                    slide.css('opacity', 0);
+                }
+            }
+        },
+        onSetTransition: function(swiper, transition) {
+            for (var i = 0; i < swiper.slides.length; i++) {
+                var slide = swiper.slides.eq(i)
+                slide.transition(transition);
+            }
+
+        },
+        onSlideChangeStart: function(swiper) {
+            if (swiper.activeIndex == 4) {
+                swiper.bullets.eq(9).addClass('swiper-pagination-bullet-active');
+                console.log(swiper.bullets.length);
+            }
+        }
+    });
+
+
+
+
 
 });
 
